81c93b2bfaf67eda58a9eea0271f589a83c4c974
jjjcoutu <jjjcoutu@gmail.com>
2015-02-06 20:34:03 -0500

    a semicolon is not actually half a colon

--- makeGrid2.m
+++ makeGrid2.m
@@ -1,9 +1,9 @@
 function [gridXValues, gridYValues, classifiedValues] = makeGrid2(gridStep, dataA, dataB)
 
-    xMin = min([min(dataA(:,1)) min(dataB(:,1))])
-    xMax = max([max(dataA(:,1)) max(dataB(:,1))])
-    yMin = min([min(dataA(:,2)) min(dataB(:,2))])
-    yMax = max([max(dataA(:,2)) max(dataB(:,2))])
+    xMin = min([min(dataA(:,1)) min(dataB(:,1))]);
+    xMax = max([max(dataA(:,1)) max(dataB(:,1))]);
+    yMin = min([min(dataA(:,2)) min(dataB(:,2))]);
+    yMax = max([max(dataA(:,2)) max(dataB(:,2))]);
     
     gridXValues = (xMin):gridStep:(xMax);
     gridYValues = (yMin):gridStep:(yMax);
--- makeGrid3.m
+++ makeGrid3.m
@@ -1,9 +1,9 @@
 function [gridXValues, gridYValues, classifiedValues] = makeGrid3(gridStep, dataA, dataB, dataC)
 
-    xMin = min([min(dataA(:,1)) min(dataB(:,1)) min(dataC(:,1))])
-    xMax = max([max(dataA(:,1)) max(dataB(:,1)) max(dataC(:,1))])
-    yMin = min([min(dataA(:,2)) min(dataB(:,2)) min(dataC(:,2))])
-    yMax = max([max(dataA(:,2)) max(dataB(:,2)) max(dataC(:,2))])
+    xMin = min([min(dataA(:,1)) min(dataB(:,1)) min(dataC(:,1))]);
+    xMax = max([max(dataA(:,1)) max(dataB(:,1)) max(dataC(:,1))]);
+    yMin = min([min(dataA(:,2)) min(dataB(:,2)) min(dataC(:,2))]);
+    yMax = max([max(dataA(:,2)) max(dataB(:,2)) max(dataC(:,2))]);
     
     gridXValues = (xMin):gridStep:(xMax);
     gridYValues = (yMin):gridStep:(yMax);
